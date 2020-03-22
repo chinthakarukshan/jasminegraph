2026962b9d1d09cea4d8168e60240c5ec88d7256
chinthaka <chinthakarukshan@gmail.com>
2020-03-22 10:28:22 +0530

    Adding logs for troubleshooting

--- src/server/JasmineGraphInstanceService.cpp
+++ src/server/JasmineGraphInstanceService.cpp
@@ -1058,8 +1058,10 @@ void JasmineGraphInstanceService::loadInstanceCentralStore(std::string graphId, 
 }
 
 JasmineGraphHashMapCentralStore JasmineGraphInstanceService::loadCentralStore(std::string centralStoreFileName) {
+    instance_logger.log("###INSTANCE### Loading Central Store File : Started " + centralStoreFileName,"info");
     JasmineGraphHashMapCentralStore *jasmineGraphHashMapCentralStore = new JasmineGraphHashMapCentralStore();
     jasmineGraphHashMapCentralStore->loadGraph(centralStoreFileName);
+    instance_logger.log("###INSTANCE### Loading Central Store File : Completed","info");
     return *jasmineGraphHashMapCentralStore;
 }
 
@@ -1143,15 +1145,16 @@ long JasmineGraphInstanceService::aggregateCentralStoreTriangles(std::string gra
 
     instance_logger.log("###INSTANCE### Loading Aggregator File Location : Started","info");
     DIR* dirp = opendir(aggregatorFilePath.c_str());
-    instance_logger.log("###INSTANCE### Loading Aggregator File Location : Completed","info");
     struct dirent * dp;
     while ((dp = readdir(dirp)) != NULL) {
         fileNames.push_back(dp->d_name);
     }
     closedir(dirp);
+    instance_logger.log("###INSTANCE### Loading Aggregator File Location : Completed","info");
 
     std::vector<std::string>::iterator fileNamesIterator;
 
+    instance_logger.log("###INSTANCE### Central Store Aggregation : Started","info");
     for (fileNamesIterator = fileNames.begin(); fileNamesIterator != fileNames.end(); ++fileNamesIterator) {
         std::string fileName = *fileNamesIterator;
         struct stat s;
@@ -1174,6 +1177,8 @@ long JasmineGraphInstanceService::aggregateCentralStoreTriangles(std::string gra
         }
 
     }
+    instance_logger.log("###INSTANCE### Central Store Aggregation : Completed","info");
+
     map<long, long> distributionHashMap = JasmineGraphInstanceService::getOutDegreeDistributionHashMap(aggregatedCentralStore);
 
     long aggregatedTriangleCount = Triangles::countCentralStoreTriangles(aggregatedCentralStore,distributionHashMap);
