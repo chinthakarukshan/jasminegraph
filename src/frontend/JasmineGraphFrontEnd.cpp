c223d22c20fc7315f04c8689ede9790d3b0cdcb9
chinthaka <chinthakarukshan@gmail.com>
2020-03-25 23:38:59 +0530

    Fixed host resolving issue

--- src/frontend/JasmineGraphFrontEnd.cpp
+++ src/frontend/JasmineGraphFrontEnd.cpp
@@ -869,7 +869,7 @@ long JasmineGraphFrontEnd::getTriangleCount(int graphId, std::string host, int p
     }
 
     if (host.find('@') != std::string::npos) {
-        host = utils.split(host, '@')[0];
+        host = utils.split(host, '@')[1];
     }
 
     server = gethostbyname(host.c_str());
